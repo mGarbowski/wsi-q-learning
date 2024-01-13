03eeb441ffa28b7696b7272e8c2835070cc147a8
mgarbowski <mikolaj.garbowski@gmail.com>
2024-01-13 21:42:11 +0100

    do not use poetry

--- main.py
+++ main.py
@@ -4,35 +4,17 @@ import numpy as np
 
 
 def main():
-    env = gym.make('FrozenLake-v1', desc=None, map_name="8x8", is_slippery=False)
-    state_size = env.observation_space.n
-    action_size = env.action_space.n
+    env = gym.make('FrozenLake-v1', desc=None, map_name="8x8", is_slippery=False, render_mode="human")
+    observation, info = env.reset()
 
-    num_of_ind_runs = 25
-    num_episodes = 1000
-    averaged_reward = np.zeros(num_episodes)
-    for run in range(num_of_ind_runs):
-        qtable = np.zeros((state_size, action_size))
-        ...
-        for episode in range(num_episodes):
-            ...
-            averaged_reward[episode] = averaged_reward[episode] + reward
-            ...
-    averaged_reward = averaged_reward / (num_of_ind_runs)
-    averaged_reward_base = averaged_reward  # niech to będą wyniki bazowe, z którymi będziemy porównywać wyniki dla innych ustawień, czy funkcji oceny
+    for _ in range(1000):
+        action = env.action_space.sample()  # agent policy that uses the observation and info
+        observation, reward, terminated, truncated, info = env.step(action)
 
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1)
-    ax.spines['left'].set_position('center')
-    ax.spines['bottom'].set_position('zero')
-    ax.spines['right'].set_color('none')
-    ax.spines['top'].set_color('none')
-    ax.xaxis.set_ticks_position('bottom')
-    ax.yaxis.set_ticks_position('left')
-    plt.plot(averaged_reward_base, 'r')
-    plt.plot(averaged_reward, 'b')
-    plt.show()
+        if terminated or truncated:
+            observation, info = env.reset()
 
+    env.close()
 
 if __name__ == '__main__':
     main()
